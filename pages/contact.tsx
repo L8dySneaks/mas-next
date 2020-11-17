cb8e218a518692d19bbfd508550eab53c1bb0d6c
Steph Symosko <steph.symosko@gmail.com>
2020-11-16 21:33:38 -0500

    example with using variables with CSS

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,3 +1,27 @@
+/**
+ * Styled JSX Component
+ * JSX Styles are like inline styles, they are component level
+ * Can make the style global by adding the global keyword after jsx in the style tag
+ */
+
+function Heading(props) {
+  const variableColor = Math.random() > 0.5 ? 'red' : 'blue';
+  return <div><h1>{props.heading}</h1>
+    <style jsx>
+      {`
+        h1 {
+          color: ${variableColor};
+        }
+      `}
+    </style>
+  </div>
+}
+
 export default function Contact() {
-  return <h1>Contact Me!</h1>
+  return (
+    <div>
+      <Heading heading="Heading" />
+      <h1>Here</h1>
+    </div>
+  )
 }
