ac8688bd30335d871994f8d874aa19aa5a1c52b7
Steph Symosko <steph.symosko@gmail.com>
2020-11-22 15:31:25 -0500

    example of using the revalidate param

--- pages/dynamic.tsx
+++ pages/dynamic.tsx
@@ -7,7 +7,11 @@ export const getStaticProps: GetStaticProps = async (context) => {
   const fs = require('fs');
   const txt = fs.readFileSync(path.join(process.cwd(), 'public/robots.txt'), 'utf-8');
   // takes this return value and injects it in to the props of the component function below
+  // Can use the revalidate param in order to try and update the static file that gets 
+  // created when the NextJS build is run, can only run this on Production
+  // revalidate: 10, every 10 seconds it will look for new content on the static file and re-populate as needed
   return {
+    revalidate: 10,
     props: {
       myFavNum: txt
     }
