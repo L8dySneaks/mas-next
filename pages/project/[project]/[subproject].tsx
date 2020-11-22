34f534b851141d734a4b6baeae3016e15ca3197d
Steph Symosko <steph.symosko@gmail.com>
2020-11-22 14:14:15 -0500

    useRouter example

--- pages/project/[project]/[subproject].tsx
+++ pages/project/[project]/[subproject].tsx
@@ -4,6 +4,27 @@
 * Lose the ability to capture 2 dynamic segments
 */
 
+import { useRouter } from 'next/router';
+
 export default function SubProjectName() {
-  return <h1>Hello from second segment</h1>
+
+  const router = useRouter();
+  console.log(router);
+
+  function takeMeHome() {
+    // This pushed home onto the stack of history, so therefor the back button on the browser works
+    router.push('/');
+    // The back button will take you back to whatever the original page was you came from, not where you JUST came from
+    //router.replace('/');
+  }
+
+  /**
+   * NextJS will make the query request blank on the first time around, unless you have prefetching
+   * requests, like getServerSideProps or getStaticProps
+   */
+
+  return <h1>
+    Hello {router.query.project} {router.query.subproject}
+    <button onClick={takeMeHome}>Home</button>
+  </h1>
 }
