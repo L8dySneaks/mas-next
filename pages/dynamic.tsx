186db0bc4019d3a7fe3c8084696656ee974c6f89
Steph Symosko <steph.symosko@gmail.com>
2020-11-22 15:56:55 -0500

    example of getServerSideProps

--- pages/dynamic.tsx
+++ pages/dynamic.tsx
@@ -1,19 +1,13 @@
-import { GetStaticProps } from "next"
-import path from 'path'
+import { GetServerSideProps } from "next"
 
 
-// This function executes on server
-export const getStaticProps: GetStaticProps = async (context) => {
-  const fs = require('fs');
-  const txt = fs.readFileSync(path.join(process.cwd(), 'public/robots.txt'), 'utf-8');
-  // takes this return value and injects it in to the props of the component function below
-  // Can use the revalidate param in order to try and update the static file that gets 
-  // created when the NextJS build is run, can only run this on Production
-  // revalidate: 10, every 10 seconds it will look for new content on the static file and re-populate as needed
-  return {
-    revalidate: 10,
+// This function executes on server always on every page request
+// That is one reason why this is not a good practice, because this calls a node server on every request
+// no caching benefits, or requests will be slow
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  return{
     props: {
-      myFavNum: txt
+      myFavNum: Math.random()
     }
   }
 }
